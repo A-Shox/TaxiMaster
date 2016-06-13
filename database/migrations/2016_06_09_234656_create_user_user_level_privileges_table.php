14b436b7e969fbcb74f0fc02ec39a94613390d6f
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2016-06-13 17:05:49 +0530

    user level privileges table chnaged

--- database/migrations/2016_06_09_234656_create_user_user_level_privileges_table.php
+++ database/migrations/2016_06_09_234656_create_user_user_level_privileges_table.php
@@ -13,9 +13,10 @@ class CreateUserUserLevelPrivilegesTable extends Migration
     public function up()
     {
         Schema::create('user_level_privileges', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('user_level_id');
             $table->integer('privilege_id');
-            $table->primary(array('user_level_id', 'privilege_id'));
+            $table->unique(array('user_level_id', 'privilege_id'));
         });
     }
 
