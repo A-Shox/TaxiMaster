b6b030fe8e56ff50a9e11fe98a436362cac930af
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2016-06-14 06:13:15 +0530

    test.blade changed.

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,3 +1,6 @@
 <?PHP
-$response = \App\Http\Controllers\OneSignalController::sendMessage("Title", "message", array('data'=>'test'), "bd3b492c-59de-4d5f-9ed9-05c7790dc37b", 'DRIVER');
-print_r($response);
+$response1 = \App\Http\Controllers\OneSignalController::sendMessage("Title", "message", array('data'=>'test'), "bd3b492c-59de-4d5f-9ed9-05c7790dc37b", 'DRIVER');
+$response2 = \App\Http\Controllers\OneSignalController::sendMessage("Title", "message", array('data'=>'test'), "aac7990a-77ac-4b63-9ef3-664ef962e453", 'DRIVER');
+
+print_r($response1);
+print_r($response2);
